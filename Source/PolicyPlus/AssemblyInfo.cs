1e397e14fa3dfbf33749559c03126c1ae70cdc08
JordanJWhite <127838625+JordanJWhite@users.noreply.github.com>
2025-06-16 01:27:02 +0100

    Upgrade to .NET 9 (#6)

---
+++ Source/PolicyPlus/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Policy Plus")]
+[assembly: AssemblyDescription("Local Group Policy Editor plus more, for all Windows editions. Policy Plus makes Group Policy settings available to everyone, including on Home editions. View, edit, and manage Registry-based policies with a modern interface.")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © Ben Nordick, Jordan White")]
+[assembly: AssemblyProduct("Policy Plus")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
